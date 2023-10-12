096eefb45bf637716df9fc9c101717a75d0c8c40
Tkachenko88 <vanes_09@inbox.ru>
2023-10-12 21:05:35 +0300

    header-slider

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 /* в этот файл добавляет скрипты*/
+
 //mobile menu
 let header = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
@@ -11,13 +12,58 @@ navToggle.addEventListener("click", function () {
 });
 
 //hero-slider
-var left = document.querySelector('.hero-slider__button--previous');
-var right = document.querySelector('.hero-slider__button--next');
-var viewport = document.querySelector('.hero-card');
+const prev = document.querySelector('.hero-slider__button--previous');
+const next = document.querySelector('.hero-slider__button--next');
+const cards = document.querySelectorAll('.hero-card');
+const maxCount = cards.length-1;
+let currentSlide = 0;
 
-left.addEventListener('click', function() {
+const setActiveCard = (index) => {
+  cards.forEach((card) => card.classList.remove('hero-card--active'));
+  cards[index].classList.add('hero-card--active');
+}
+
+prev.setAttribute('disabled', '')
+
+prev.addEventListener('click', function () {
+  if (currentSlide === 0) {
+    return
+  }
+  currentSlide = currentSlide - 1;
+  if (currentSlide === 0) {
+    prev.setAttribute('disabled', '')
+  }
+  if (currentSlide !== maxCount) {
+    next.removeAttribute('disabled')
+  }
+  setActiveCard(currentSlide);
 
 });
-right.addEventListener('click', function() {
 
+
+next.addEventListener('click', function () {
+  if (currentSlide === maxCount) {
+    return
+  }
+  console.log(currentSlide)
+  currentSlide = currentSlide + 1;
+  if (currentSlide === maxCount) {
+    next.setAttribute('disabled', '')
+  }
+  if (currentSlide !== 0) {
+    prev.removeAttribute('disabled')
+  }
+  setActiveCard(currentSlide);
+});
+
+//range slider
+var slider = document.querySelector('.range-bar');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
 });
