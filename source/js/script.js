6f567b5e86148ab5c07a82480cebb2bcc72c26c6
Tkachenko88 <vanes_09@inbox.ru>
2023-10-12 09:14:59 +0300

    checkbox

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 /* в этот файл добавляет скрипты*/
+//mobile menu
 let header = document.querySelector(".main-nav");
 let navToggle = document.querySelector(".main-nav__toggle");
 let navList = document.querySelector(".site-list");
@@ -7,4 +8,16 @@ let navList = document.querySelector(".site-list");
 
 navToggle.addEventListener("click", function () {
   navList.classList.toggle("site-list--closed");
+});
+
+//hero-slider
+var left = document.querySelector('.hero-slider__button--previous');
+var right = document.querySelector('.hero-slider__button--next');
+var viewport = document.querySelector('.hero-card');
+
+left.addEventListener('click', function() {
+
+});
+right.addEventListener('click', function() {
+
 });
